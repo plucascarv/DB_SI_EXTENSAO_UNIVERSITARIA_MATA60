fb59fcd11ac4d8398e8644c90a8de410957ddfb9
Pedro Carvalho <plucas682@gmail.com>
2025-11-03 11:47:12 -0300

    fix: erros de sintaxe

--- prj1_mata60.sql
+++ prj1_mata60.sql
@@ -1,45 +1,46 @@
 CREATE TABLE PARTICIPANTE(
-  id_participante INT NOT NULL,
+  id_participante INT PRIMARY KEY,
   primeiro_nome VARCHAR(50) NOT NULL,
   nome_meio VARCHAR(50),
   ultimo_nome VARCHAR(50) NOT NULL,
-  cpf_participante CHAR(11) PRIMARY KEY,
+  cpf_participante CHAR(11) UNIQUE,
   data_nascimento DATE,
-  endereço VARCHAR(150),
-  gênero CHAR(1),
-  tipo_participação CHAR(1),
-  matrícula VARCHAR(9),
-  instituição VARCHAR(50)
+  endereco VARCHAR(150),
+  genero CHAR(1),
+  tipo_participacao CHAR(1),
+  matricula VARCHAR(9) UNIQUE,
+  instituicao VARCHAR(50)
 );
 
 CREATE TABLE ATIVIDADE (
   id_atividade INT PRIMARY KEY,
-  data DATE NOT NULL,
-  horário TIME NOT NULL,
-  local VARCHAR(150) NOT NULL,
+  data_atividade DATE NOT NULL,
+  horario TIME NOT NULL,
+  local_atividade VARCHAR(150) NOT NULL,
   tipo_atividade CHAR(1) NOT NULL,
-  área_estudo VARCHAR(50) NOT NULL,
+  area_estudo VARCHAR(50) NOT NULL,
   nome_atividade VARCHAR(150) NOT NULL,
-  carga_horária INT(2) NOT NULL,
-  descrição VARCHAR(400) NOT NULL,
-  relatório_atividade VARCHAR(400)
+  carga_horaria INT NOT NULL,
+  descricao VARCHAR(400) NOT NULL,
+  relatorio_atividade VARCHAR(400)
 );
 
 CREATE TABLE PARTICIPA (
   feedback VARCHAR(400),
-  certificação CHAR(1),
-  cpf_participante CHAR(11) PRIMARY KEY,
-  id_atividade VARCHAR(150) NOT NULL,
-  FOREIGN KEY (cpf_participante) REFERENCES PARTICIPANTE(cpf_participante),
+  certificacao CHAR(1),
+  id_participante INT,
+  id_atividade INT,
+  PRIMARY KEY (id_participante, id_atividade),
+  FOREIGN KEY (id_participante) REFERENCES PARTICIPANTE(id_participante),
   FOREIGN KEY (id_atividade) REFERENCES ATIVIDADE(id_atividade)
 );
 
 CREATE TABLE PARCEIRO (
-  id_parceiro INT NOT NULL,
+  id_parceiro INT PRIMARY KEY,
   primeiro_nome VARCHAR(50) NOT NULL,
   nome_meio VARCHAR(50),
   ultimo_nome VARCHAR(50) NOT NULL,
-  cpf_representante VARCHAR(11) PRIMARY KEY,
+  cpf_representante VARCHAR(11) UNIQUE,
   empresa VARCHAR(150) NOT NULL,
   categoria_parceiro CHAR(1) NOT NULL,
   id_atividade INT NOT NULL,
