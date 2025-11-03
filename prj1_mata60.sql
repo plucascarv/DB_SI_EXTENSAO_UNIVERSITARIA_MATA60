7091d971dc83e5ce6211a5185373929ec98727ff
Pedro Carvalho <plucas682@gmail.com>
2025-11-02 22:35:09 -0300

    fix: tipagem de atributos e remoção de atributos com PK desnecessário

--- prj1_mata60.sql
+++ prj1_mata60.sql
@@ -1,25 +1,25 @@
 CREATE TABLE PARTICIPANTE(
-  id_participante INT,
+  id_participante INT NOT NULL,
   primeiro_nome VARCHAR(50) NOT NULL,
   nome_meio VARCHAR(50),
   ultimo_nome VARCHAR(50) NOT NULL,
-  cpf CHAR(11) PRIMARY KEY,
+  cpf_participante CHAR(11) PRIMARY KEY,
   data_nascimento DATE,
   endereço VARCHAR(150),
   gênero CHAR(1),
   tipo_participação CHAR(1),
-  matrícula VARCHAR(9) PRIMARY KEY,
+  matrícula VARCHAR(9),
   instituição VARCHAR(50)
 );
 
 CREATE TABLE ATIVIDADE (
-  id_atividade INT,
+  id_atividade INT PRIMARY KEY,
   data DATE NOT NULL,
   horário TIME NOT NULL,
   local VARCHAR(150) NOT NULL,
-  tipo_atividade VARCHAR(50)
+  tipo_atividade CHAR(1) NOT NULL,
   área_estudo VARCHAR(50) NOT NULL,
-  nome_atividade VARCHAR(150) PRIMARY KEY,
+  nome_atividade VARCHAR(150) NOT NULL,
   carga_horária INT(2) NOT NULL,
   descrição VARCHAR(400) NOT NULL,
   relatório_atividade VARCHAR(400)
@@ -29,19 +29,19 @@ CREATE TABLE PARTICIPA (
   feedback VARCHAR(400),
   certificação CHAR(1),
   cpf_participante CHAR(11) PRIMARY KEY,
-  nome_atividade VARCHAR(150) PRIMARY KEY,
+  id_atividade VARCHAR(150) NOT NULL,
   FOREIGN KEY (cpf_participante) REFERENCES PARTICIPANTE(cpf_participante),
-  FOREIGN KEY (nome_atividade) REFERENCES ATIVIDADE(nome_atividade)
-)
+  FOREIGN KEY (id_atividade) REFERENCES ATIVIDADE(id_atividade)
+);
 
 CREATE TABLE PARCEIRO (
-  id_parceiro INT,
+  id_parceiro INT NOT NULL,
   primeiro_nome VARCHAR(50) NOT NULL,
   nome_meio VARCHAR(50),
   ultimo_nome VARCHAR(50) NOT NULL,
-  cpf_representante VARCHAR(11) PRIMARY NOT NULL,
+  cpf_representante VARCHAR(11) PRIMARY KEY,
   empresa VARCHAR(150) NOT NULL,
-  categoria_parceiro CHAR(1) NOT NULL
-  id_atividade INT,
+  categoria_parceiro CHAR(1) NOT NULL,
+  id_atividade INT NOT NULL,
   FOREIGN KEY (id_atividade) REFERENCES ATIVIDADE(id_atividade)
 );
